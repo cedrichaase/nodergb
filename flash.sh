f18cf713b084565f036a6cde146b74c6bb5fe9c0
Cedric Haase <cedric@sineband.de>
2017-05-11 02:42:09 +0200

    Remove firmware-related files

--- flash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-which esptool.py || exit 1
-
-esptool.py --port=/dev/cu.wchusbserial1420 erase_flash
-esptool.py --port=/dev/cu.wchusbserial1420 write_flash -fm=dio -fs=32m 0x00000 ./nodemcu-master-9-modules-2017-05-10-13-11-01-float.bin
-
