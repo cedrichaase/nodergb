a8bb41e13e0ce05b1e69ddedd38e86b65801ccf7
Cedric Haase <cedric@sineband.de>
2017-05-11 02:32:30 +0200

    Add tcp service and fix some bugs

--- init.lua
+++ init.lua
@@ -6,20 +6,23 @@ PIN_BLUE    = 3
 -- LED PWM settings (1-1000)
 PWM_FREQ = 500
 
-
 -- WIFI settings
 WIFI_SSID = "your-ssid-here"
 WIFI_PASS = "changeme"
 
 -- RGB Service settings
-SERVICE_UDP_PORT = 1338
-SERVICE_HTTP_PORT = 80
+SERVICE_UDP_PORT = 1337
+SERVICE_TCP_PORT = 1338
 
+-- trims a string
+function string_trim(s)
+  return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
 
 -- sets the duty cycle with a max value of 255
 set_duty_8bit = function(pin, duty)
-    local duty_8bit = duty * 3
-    pwm.setduty(pin, duty_8bit)
+    if duty == nil then return end
+    pwm.setduty(pin, duty * 4)
 end
 
 set_color = function(red, green, blue)
@@ -36,17 +39,16 @@ set_color_hex = function(hexcolor)
 
     r, g, b = 0, 0, 0
     
-    if length == 7 then
+    if length == 6 then
         r = string.sub(hexcolor, 1, 2)
         g = string.sub(hexcolor, 3, 4)
         b = string.sub(hexcolor, 5, 6)
-
-        print(r, g, b)
-    elseif length == 4 then
+    elseif length == 3 then
         r = string.rep(string.sub(hexcolor, 1, 1), 2)
         g = string.rep(string.sub(hexcolor, 2, 2), 2)
         b = string.rep(string.sub(hexcolor, 3, 3), 2)
-    elseif length == 2 then
+    elseif length == 1 then
+        hexcolor = string.sub(hexcolor, 1, 1)
         r, g, b = string.rep(hexcolor, 2), string.rep(hexcolor, 2), string.rep(hexcolor, 2)
     end
 
@@ -67,8 +69,19 @@ pwm.start(PIN_BLUE)
 wifi.setmode(wifi.STATION)
 wifi.sta.config(WIFI_SSID, WIFI_PASS)
 
-socket=net.createServer(net.UDP)
+-- setup udp service
+socket=net.createUDPSocket()
 socket:on("receive", function(sck, payload)
-    set_color_hex(payload)
+    set_color_hex(string_trim(payload))
 end)
 socket:listen(SERVICE_UDP_PORT)
+
+-- setup tcp service
+server=net.createServer(net.TCP)
+server:listen(SERVICE_TCP_PORT, function(conn)
+    conn:on("receive", function(conn, payload)
+        payload = string_trim(payload)
+        if payload == "q" then conn:close(); return end 
+        set_color_hex(payload)
+    end)
+end)
