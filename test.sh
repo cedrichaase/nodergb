d4039623df522c8d80ff8224a70add8bb51e757d
Simon Gansen <simon@sineband.de>
2017-07-21 17:02:15 +0200

    Test all pwm channels

--- test.sh
+++ test.sh
@@ -2,16 +2,53 @@
 
 IP=192.168.178.101
 PORT=1337
-SLEEP_DURATION=0.05
+SLEEP_DURATION=0.01
 
 while true; do
-    for brightness in {0..9}; do
-        echo "$brightness" | nc -cu $IP $PORT
+
+    # dim red up and down
+    for b in {0..255}; do
+        printf "%02x%02x%02x\n" $b 0 0 | nc -cu $IP $PORT
         sleep $SLEEP_DURATION
     done
-    for brightness in {9..0}; do
-        echo "$brightness" | nc -cu $IP $PORT
+
+    for b in {254..0}; do
+        printf "%02x%02x%02x\n" $b 0 0 | nc -cu $IP $PORT
         sleep $SLEEP_DURATION
     done
+
+    # dim green up and down
+    for b in {0..255}; do
+        printf "%02x%02x%02x\n" 0 $b 0 | nc -cu $IP $PORT
+        sleep $SLEEP_DURATION
+    done
+
+    for b in {254..0}; do
+        printf "%02x%02x%02x\n" 0 $b 0 | nc -cu $IP $PORT
+        sleep $SLEEP_DURATION
+    done
+
+    # dim blue up and down
+    for b in {0..255}; do
+        printf "%02x%02x%02x\n" 0 0 $b | nc -cu $IP $PORT
+        sleep $SLEEP_DURATION
+    done
+
+    for b in {254..0}; do
+        printf "%02x%02x%02x\n" 0 0 $b | nc -cu $IP $PORT
+        sleep $SLEEP_DURATION
+    done
+
+    # dim all up and down
+    for b in {0..255}; do
+        printf "%02x%02x%02x\n" $b $b $b | nc -cu $IP $PORT
+        sleep $SLEEP_DURATION
+    done
+
+    for b in {254..0}; do
+        printf "%02x%02x%02x\n" $b $b $b | nc -cu $IP $PORT
+        sleep $SLEEP_DURATION
+    done
+
 done
 
