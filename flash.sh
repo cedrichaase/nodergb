c7180a9c87e0f1a98c6e6b88df451b38513df19f
Cedric Haase <cedric@sineband.de>
2017-07-22 12:56:50 +0200

    Make flash script more configurable

--- flash.sh
+++ flash.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-DEV=1420
+PORT=/dev/tty.SLAB_USBtoUART
 
 which esptool.py || exit 1
 
-esptool.py --port="/dev/cu.wchusbserial${DEV}" erase_flash
-esptool.py --port="/dev/cu.wchusbserial${DEV}" write_flash -fm=dio -fs=32m 0x00000 ./nodemcu-master-9-modules-2017-05-10-13-11-01-float.bin
+esptool.py --port="${PORT}" erase_flash
+esptool.py --port="${PORT}" write_flash -fm=dio -fs=32m 0x00000 ./nodemcu-master-9-modules-2017-05-10-13-11-01-float.bin
 
